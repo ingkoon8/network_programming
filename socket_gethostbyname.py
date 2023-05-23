6ad0a729280e05c7e5c35cc2a728f7f4f5905edd
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-05-23 18:43:47 +0900

    Update socket_gethostbyname.py

--- socket_gethostbyname.py
+++ socket_gethostbyname.py
@@ -1,18 +1,27 @@
-import socket
+from socket import *
 HOSTS = [ 'www.naver.com', 'www.google.com', 'www.python.org','testname']
 
 for host in HOSTS:
     print(host)
     try:
-       hostname, aliases, addresses = socket.gethostbyname_ex(host)
+       hostname, aliases, addresses = gethostbyname_ex(host)
        print('Hostname : ',hostname)
        print('Aliases : ',aliases)
        print('Addresses : ',addresses)
     # host 찾을 수 없을 경우 error
-    except socket.error as e_msg:
+    except error as e_msg:
         print('{} : {}'.format(host, e_msg))
     print()
 
 # 나의 host 정보 가져오기
-myhost = socket.gethostname()
-print(socket.gethostbyname_ex(myhost))
+myhost = gethostname()
+print(gethostbyname_ex(myhost))
+
+# 완전한 도메인 확인
+print(getfqdn('pymotw.com'))
+
+# ip 주소로 호스트 정보 가져오기
+hostname2, aliases2, addresses2 = gethostbyaddr('203.249.39.46')
+print('Hostname : ',hostname2)
+print('Aliases : ',aliases2)
+print('Addresses : ',addresses2)
