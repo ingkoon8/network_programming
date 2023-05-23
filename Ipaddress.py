917846afd4f8bf2907e91e622cdba5613102f7dc
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-05-23 17:05:27 +0900

    Update Ipaddress.py

--- Ipaddress.py
+++ Ipaddress.py
@@ -5,9 +5,9 @@ Address = ['192.168.0.5', '2001:0:9d38:6abd:480:f1f:3f57:fffb']
 
 for ipaddr in Address:
     addr = ipa.ip_address(ipaddr)
-    print(f'IP address : {addr!r}')
+    print(f'IP address : {addr!r}') 
     print('IP version : ',addr.version)
-    print('Packed addr : ',binascii.hexlify(addr.packed))
+    print('Packed addr : ',binascii.hexlify(addr.packed)) # 16진수로 표현
     print('Integer addr : ',int(addr))
-    print('Is private? : ',addr.is_private)
+    print('Is private? : ',addr.is_private) # 사설 네트워크인지 확인
     print()
