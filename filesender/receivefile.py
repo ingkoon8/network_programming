0abbc4224337dd83394758a8198e047eb9a16bf0
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-05-29 22:48:30 +0900

    Update receivefile.py

--- filesender/receivefile.py
+++ filesender/receivefile.py
@@ -8,14 +8,14 @@ s_sock.connect((host, port))
 s_sock.send('Ready'.encode())
 fn = s_sock.recv(1024).decode()
 
-with open('/Users/yujaemin/Desktop/python/network2/'+fn, 'wb') as f: # 보낼 path 지정
+with open('/Users/yujaemin/Desktop/python/network2/'+fn, 'wb') as f: # 전송 받을 path 지정
     print('file opened')
     print('receiving file...')
     while 1:
-        data = s_sock.recv(8192)
+        data = s_sock.recv(8192) # 파일 내용 수신
         if not data:
             break
-        f.write(data)
+        f.write(data) # 내용을 파일에 쓰기
 print('Download complete')
 s_sock.close()
 print('Connection closed')
