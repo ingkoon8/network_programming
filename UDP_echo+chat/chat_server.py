d5178d33326764f9a654752919ac45541bfa6eba
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-05-30 04:14:07 +0900

    Create chat_server.py

---
+++ UDP_echo+chat/chat_server.py
@@ -0,0 +1,14 @@
+import socket
+
+s_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP 소켓
+s_sock.bind(('', 2500))
+print('Waiting for connection...')
+
+while 1:
+    data, addr = s_sock.recvfrom(1024) # 데이터와 상대방 종단점 주소 수신
+    print()
+    print('Receive from ',addr)
+    print('Received message : ', data.decode())
+    print()
+    msg = input('Sending message : ')
+    s_sock.sendto(msg.encode(), addr)
