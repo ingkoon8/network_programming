475cb286ccabc9b7e268744a0f790d99a709d70e
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-06-05 09:50:00 +0900

    Update threading_client.py

--- thread/threading_client.py
+++ thread/threading_client.py
@@ -1,22 +1,6 @@
 import socket
 import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 서버 주소 입력
-serverIp = input(('Server IP(default : 127.0.0.1): '))
-if serverIp == '':
-    serverIp = '127.0.0.1'
-# 포트 입력
-port = input('port(default : 2500) : ')
-if port == '':
-    port = 2500
-else:
-    port=int(port)
-
-s.connect((serverIp, port))
-print('Connected to ' + serverIp)
-
 def send(s):
     while 1:
         msg = input()
@@ -28,12 +12,28 @@ def receive(s):
         print(f'Received message : {msg.decode()}')
 
 
-Thread1 = threading.Thread(target = send, args = (s,)) # 서브스레드 생성
-Thread1.start()
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-Thread2 = threading.Thread(target = receive, args = (s,)) # 서브스레드 생성
-Thread2.start()
+    # 서버 주소 입력
+    serverIp = input(('Server IP(default : 127.0.0.1): '))
+    if serverIp == '':
+        serverIp = '127.0.0.1'
+    # 포트 입력
+    port = input('port(default : 2500) : ')
+    if port == '':
+        port = 2500
+    else:
+        port=int(port)
 
+    s.connect((serverIp, port))
+    print('Connected to ' + serverIp)
+
+    Thread1 = threading.Thread(target = send, args = (s,)) # 서브스레드 생성
+    Thread1.start()
+
+    Thread2 = threading.Thread(target = receive, args = (s,)) # 서브스레드 생성
+    Thread2.start()
     
 
     
